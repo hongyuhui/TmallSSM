28a4c9261d467442fb29d4590583b274fee406c1
Codemupf <mupengfei6688@163.com>
2017-11-09 15:40:00 +0800

    基于MAVEN 搭建SSM开发基本框架

---
+++ src/main/java/com/tmall/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.tmall.mapper;
+
+import com.tmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by OovEver on 2017/11/9.
+ */
+public interface CategoryMapper {
+    List<Category> list();
+}
---
+++ src/main/java/com/tmall/pojo/Category.java
@@ -0,0 +1,26 @@
+package com.tmall.pojo;
+
+/**
+ * Created by OovEver on 2017/11/9.
+ */
+public class Category {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
