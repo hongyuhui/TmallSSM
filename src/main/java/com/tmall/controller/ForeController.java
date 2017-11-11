a72ef0a5ea4b16c6893e20d113d1367f234aff61
Codemupf <mupengfei6688@163.com>
2017-11-11 23:48:35 +0800

    添加分类展示及按照销量、日期、综合、人气排名显示功能

--- src/main/java/com/tmall/controller/ForeController.java
+++ src/main/java/com/tmall/controller/ForeController.java
@@ -2,6 +2,7 @@ package com.tmall.controller;
 
 import com.tmall.pojo.*;
 import com.tmall.service.*;
+import comparator.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -120,6 +122,39 @@ public class ForeController {
         }
         session.setAttribute("user", user);
         return "success";
+    }
+
+    @RequestMapping("forecategory")
+    public String category(int cid, String sort, Model model) {
+        Category c = categoryService.get(cid);
+        productService.fill(c);
+        productService.setSaleAndReviewNumber(c.getProducts());
+
+        if(null!=sort){
+            switch(sort){
+                case "review":
+                    Collections.sort(c.getProducts(),new ProductReviewComparator());
+                    break;
+                case "date" :
+                    Collections.sort(c.getProducts(),new ProductDateComparator());
+                    break;
+
+                case "saleCount" :
+                    Collections.sort(c.getProducts(),new ProductSaleCountComparator());
+                    break;
+
+                case "price":
+                    Collections.sort(c.getProducts(),new ProductPriceComparator());
+                    break;
+
+                case "all":
+                    Collections.sort(c.getProducts(),new ProductAllComparator());
+                    break;
+            }
+        }
+
+        model.addAttribute("c", c);
+        return "fore/category";
     }
 }
 
