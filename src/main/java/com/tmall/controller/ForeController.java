1b828fb0871261934dd358140dad2989586e43c3
Codemupf <mupengfei6688@163.com>
2017-11-12 14:23:39 +0800

    添加查看购物车功能

--- src/main/java/com/tmall/controller/ForeController.java
+++ src/main/java/com/tmall/controller/ForeController.java
@@ -235,5 +235,13 @@ public class ForeController {
         }
         return "success";
     }
+
+    @RequestMapping("forecart")
+    public String cart(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        List<OrderItem> ois = orderItemService.listByUser(user.getId());
+        model.addAttribute("ois", ois);
+        return "fore/cart";
+    }
 }
 
